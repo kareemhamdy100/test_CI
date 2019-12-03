ecb33ec8498cb5c00336b4c7031f769fba568377
kareemhamdy500 <kareemhamdy500@gmail.com>
2019-12-03 15:52:01 +0200

    some exchanges

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb+srv://falconkareem500:Engfalcon1@cluster0-l5rnw.mongodb.net/blog_dev?retryWrites=true&w=majority',
   cookieKey: '123123123'
 };
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('./models/Blog');
 require('./services/passport');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useMongoClient: true });
+mongoose.connect(keys.mongoURI,  {useNewUrlParser : true, useUnifiedTopology: true} );
 
 const app = express();
 
@@ -27,7 +27,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/blogRoutes')(app);
 
-if (['production'].includes(process.env.NODE_ENV)) {
+if (['production', 'ci'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
 
   const path = require('path');
